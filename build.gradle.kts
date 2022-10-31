5f29ecaa5817772c79fa6ed43f637db7b2614aa4
Michael Kropat <michael.can.code@kropat.email>
2022-10-31 18:39:27 -0400

    Strip down

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,8 @@ plugins {
 }
 
 group = "com.codetinkerer"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
