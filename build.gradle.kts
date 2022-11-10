d2f225691f9d68b18b8a3ce5a6d424767ad936ab
Michael Kropat <michael.can.code@kropat.email>
2022-11-09 21:31:49 -0500

    Build rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
-    id("java")
+    java
 }
 
-group = "com.codetinkerer"
+group = "com.codetinkerer.servedir"
 version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "com.codetinkerer.servedir.ServeDir"
+    val dependencies = configurations.runtimeClasspath.get()
+        .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
